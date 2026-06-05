5c3cf8b7d3cb9f562f84ca0ca86395194c25d1d8
Adam Horodyski <42047036+adhorodyski@users.noreply.github.com>
2026-06-05 16:00:43 +0200

    Update darwin.nix

--- hosts/darwin.nix
+++ hosts/darwin.nix
@@ -5,8 +5,6 @@
 
   homebrew = {
     enable = true;
-    # Each switch, uninstall + zap any brew not declared below (SSOT). --force is
-    # required by newer Homebrew to actually perform the destructive cleanup.
     onActivation.cleanup = "zap";
     onActivation.extraFlags = [ "--force" ];
     brews = [ "cocoapods" "rbenv" "watchman" ];
@@ -15,8 +13,6 @@
 
   programs.zsh.enable = true;
 
-  # nix-darwin doesn't create users; it just needs to know the home dir so
-  # home-manager can resolve home.homeDirectory.
   users.users.adhorodyski.home = "/Users/adhorodyski";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
