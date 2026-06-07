966f41600e3eccb402e2d156b6519cf77d77bb26
Adam Horodyski <ad.horodyski@gmail.com>
2026-06-08 01:18:01 +0200

    fix broadcom firmware drivers for mini

--- hosts/mini/default.nix
+++ hosts/mini/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 
 {
   imports = [
@@ -11,5 +11,6 @@
   system.stateVersion = "26.05";
 
   nixpkgs.config.allowUnfreePredicate = pkg: lib.getName pkg == "b43-firmware";
-  hardware.firmware = [ pkgs.b43Firmware_6_30_163_46 ];
+  networking.enableB43Firmware = true;
+  boot.blacklistedKernelModules = [ "wl" "brcmsmac" "brcmfmac" ];
 }
