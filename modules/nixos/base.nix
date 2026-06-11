a7952fdeea425fb2ad2cab4120065c59b7ee82e2
Adam Horodyski <ad.horodyski@gmail.com>
2026-06-11 10:15:11 +0200

    skip bootloader timeout on nixos

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -3,6 +3,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.timeout = 0;
   boot.loader.efi.canTouchEfiVariables = true;
 
   hardware.enableRedistributableFirmware = true;
