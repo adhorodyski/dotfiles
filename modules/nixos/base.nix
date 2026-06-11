f12f8a3d3809aa57c0bd5d00755dc31b72abaa38
Adam Horodyski <ad.horodyski@gmail.com>
2026-06-11 12:40:07 +0200

    quiet kernel boot

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -5,10 +5,22 @@
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.timeout = 0;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [
+    "quiet",
+    "splash",
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.plymouth.enable = true;
 
   hardware.enableRedistributableFirmware = true;
 
   networking.networkmanager.enable = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   users.users.adhorodyski = {
     isNormalUser = true;
