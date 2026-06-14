acbb98e9af614b237c4a5812d3ace5607d139ecd
Adam Horodyski <ad.horodyski@gmail.com>
2026-06-14 10:58:52 +0000

    pin dns hosts for docker

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -2,6 +2,9 @@
 
 {
   virtualisation.docker.enable = true;
+  virtualisation.docker.daemon.settings = {
+    dns = [ "1.1.1.1" "9.9.9.9" ];
+  };
   environment.systemPackages = [ pkgs.docker-compose ];
   users.users.adhorodyski.extraGroups = [ "docker" ];
 }
