7fc4e9c1a22dabf082b75b3c6b0b6970981a8eb7
Adam Horodyski <ad.horodyski@gmail.com>
2026-06-23 13:39:49 +0200

    add pass support on linux

--- modules/home/linux/default.nix
+++ modules/home/linux/default.nix
@@ -51,6 +51,8 @@
 
   programs.noctalia.enable = true;
 
+  programs.password-store.enable = true;
+
   # GL override for Intel HD 4000 (reports 4.2; Ghostty needs 4.3).
   programs.ghostty = {
     enable = true;
